6ba440d180b4736867de5b92c9904b5b1600b488
Kirill_Morozov <mr.kiriazamorozka@yandex.ru>
2022-05-25 11:31:11 +0500

    Изменил тесты на NUnit

--- DungeonProgMaster.Model/Scripts/Levels.cs
+++ DungeonProgMaster.Model/Scripts/Levels.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 
-namespace DungeonProgMaster
+namespace DungeonProgMaster.Model
 {
     public static class Levels
     {
--- DungeonProgMaster.Model/Scripts/MapData.cs
+++ DungeonProgMaster.Model/Scripts/MapData.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.IO;
 
-namespace DungeonProgMaster
+namespace DungeonProgMaster.Model
 {
     public static class MapData
     {
