8d32224bb6e59aec0652693184aefa14aa9b12eb
Kirill_Morozov <mr.kiriazamorozka@yandex.ru>
2022-05-25 10:46:37 +0500

    Создал проект тестов для Модели

---
+++ DungeonProgMaster.Model.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿лusing Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DungeonProgMaster.Model.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
